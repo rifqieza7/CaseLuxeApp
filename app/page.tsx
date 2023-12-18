aad8a998e1817f66e10187d6a18487c06018486c
eza rifqi <ezarifqi@gmail.com>
2023-12-18 18:00:30 +0700

    feat: add jumbotron and product listing with flex row (unfinished)

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     
     <main>
+      
       <section className="bg-center bg-no-repeat bg-cover bg-[url('/image/jumbotron.png')]">
         <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Case Luxe</h1>
