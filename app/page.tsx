98b4e5ed39606f425d34177f9bae9a848035fa79
eza rifqi <ezarifqi@gmail.com>
2023-12-21 14:38:00 +0700

    style: change text size

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     
 <main>
-
 <nav className="bg-white shadow fixed w-full">
   <div className="container mx-auto px-6 py-3 md:flex md:justify-between md:items-center">
     <div className="flex justify-between items-center">
@@ -21,15 +20,15 @@ export default function Home() {
     
     <div className="md:flex items-center first-letter:">
       <div className="flex flex-col md:flex-row md:mx-6 sty">
-        <a className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0" href="#">Home</a>
-        <a className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0" href="#">Shop</a>
-        <a className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0" href="#">Contact</a>
-        <a className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0" href="#">About</a>
+        <a className="my-1 text-lg text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0" href="#">Home</a>
+        <a className="my-1 text-lg text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0" href="#">Shop</a>
+        <a className="my-1 text-lg text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0" href="#">Contact</a>
+        <a className="my-1 text-lg text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0" href="#">About</a>
       </div>
 
       <div className="flex justify-center md:block">
         <a className="relative text-gray-700 hover:text-gray-600" href="#">
-          <svg className="h-5 w-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg className="h-7 w-7" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>
 
