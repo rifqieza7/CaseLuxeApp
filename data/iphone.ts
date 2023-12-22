ced991019c5d56cd867ef58576b4ea20b5677d38
Agustrihanton <agustrihanton97@gmail.com>
2023-12-22 14:30:35 +0700

    feat: add phone data pair programming with rifqi

---
+++ data/iphone.ts
@@ -0,0 +1,42 @@
+export const phoneData = [
+  {
+    id: 1,
+    image: {
+      src: '/image/caseblack.png',
+      alt: 'img-case',
+    },
+    isHot: true,
+    link: '#',
+    title: 'Iphone 15 Case Black',
+  },
+  {
+    id: 2,
+    image: {
+      src: '/image/caseblue.png',
+      alt: 'another-img-case',
+    },
+    link: '#',
+    title: 'Samsung Galaxy Case Blue',
+    isHot: false,
+  },
+  {
+    id: 3,
+    image: {
+      src: '/image/caseguava.png',
+      alt: 'yet-another-img-case',
+    },
+    isHot: false,
+    link: '#',
+    title: 'Google Pixel Case Red',
+  },
+  {
+    id: 4,
+    image: {
+      src: '/image/casetaupe.png',
+      alt: 'some-img-case',
+    },
+    isHot: true,
+    link: '#',
+    title: 'OnePlus Case Green',
+  },
+];
